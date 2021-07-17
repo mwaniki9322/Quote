cfebb4c2609481a9059258cee9dfc059fc3f15aa
Dennis Mwaniki <damos5776@gmail.com>
2021-07-17 09:20:18 +0300

    app-highlight directive

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,13 +1,21 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor(private elem:ElementRef) {
-    this.elem.nativeElement.style.background="rgb(139, 231, 250)";
+  constructor(private elem:ElementRef) {}
+
+  @HostListener ('click') onClicks(){
+    this.background('rgb(139, 231, 250)')
   }
+  @HostListener ('dblclick') onDoubleClicks(){
+    this.background('red');
+  }
+   background(action:string){
+     this.elem.nativeElement.style.background=action;
+   }
    }
 
 
