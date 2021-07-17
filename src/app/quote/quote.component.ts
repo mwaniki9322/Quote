2c36a3ac532967df1eae26021e8b22c200bca0bd
Dennis Mwaniki <damos5776@gmail.com>
2021-07-17 15:45:05 +0300

    image add

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,12 +8,12 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
 quotes:Quote[]=[
-  new Quote('Brian Kibet','Benjamin Franklin','Tell me and I forget. Teach me and I remember. Involve me and I learn.',new Date(2021,7,4)),
-  new Quote('Rachel kigal','Ralph Waldo Emerson','Do not go where the path may lead, go instead where there is no path and leave a trail.',new Date(2021,7,13)),
+  new Quote('Brian Kibet','Benjamin Franklin','Tell me and I forget. Teach me and I remember. Involve me and I learn.',new Date(2020,8,4)),
+  new Quote('Rachel kigal','Ralph Waldo Emerson','Do not go where the path may lead, go instead where there is no path and leave a trail.',new Date(2021,6,13)),
 ]
 addNewQuote(quote:any){
-  let goalLength = this.quotes.length;
-  quote.id = goalLength+1;
+  let quoteLength = this.quotes.length;
+  quote.id = quoteLength-1;
   quote.completeDate = new Date(quote.completeDate)
   this.quotes.push(quote)
 }
